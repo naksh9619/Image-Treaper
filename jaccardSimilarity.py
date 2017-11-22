c3eb5ad3479f200a313f12ce7f1c043b555f7485
Naksh Arora <naksharora9619@gmail.com>
2017-11-22 11:48:30 +0530

    Jaccard similarity added

---
+++ jaccardSimilarity.py
@@ -0,0 +1,36 @@
+#https://gist.github.com/ariezncahyo/7fa9c0a88b474a1b5f3b72e4d9650292
+#http://scikit-learn.org/stable/modules/generated/sklearn.metrics.jaccard_similarity_score.html
+#http://love-python.blogspot.in/2012/07/python-code-to-compute-jaccard-index.html
+from sklearn.metrics import jaccard_similarity_score
+from shingling import sigMatrix
+from math import * 
+import numpy as np
+#print(sigMatrix)
+#print(len(sigMatrix))
+#for i in range(sigMatrix):
+#	for j in range()
+def ComputeScore(set1 , set2):
+	#print(set1,set2)
+	Intersection = len(set(set1).intersection(set2))
+	Union = len(set(set1).union(set2))
+	score = Intersection / float(Union)
+	return score
+	
+
+def computejaccardSimilarity(signatureMatrix):
+	similarityMat = []
+	similarityMat2 = []
+	for i in range(5):
+		for j in range(i + 1 , 5):
+			set1 = signatureMatrix[i]
+			set2 = signatureMatrix[j]
+			score = ComputeScore(set1 , set2)
+			scoreauto = jaccard_similarity_score(set1 , set2)
+			similarityMat.append(score)
+			similarityMat2.append(scoreauto)
+	return similarityMat , similarityMat2
+
+
+jaccardSimilarity,jaccardSimilarityAuto = computejaccardSimilarity(sigMatrix)
+print("Manually calculated:" , jaccardSimilarity)
+print("Model calculated :" , jaccardSimilarityAuto)
--- shingling.py
+++ shingling.py
@@ -1,6 +1,7 @@
 #http://codegists.com/snippet/python/k-shingling-pythonpy_renien_python
-#http://stuartmyles.blogspot.in/2012/07/shingling-in-python.html
-#http://ethen8181.github.io/machine-learning/clustering_old/text_similarity/text_similarity.html#k-shingling
+#http://ethen8181.github.io/machine-learning/clustering_old/text_similarity/text_similarity.html#k-shingling4
+#http://dspace.thapar.edu:8080/jspui/bitstream/10266/4043/4/4043.pdf
+
 ShingleSize = 5
 def ComputeShingles(data):
 	shingles = set()
@@ -19,6 +20,7 @@ def ComputeShingles(data):
 		#return shingleArr
 		return shingles
 
+
 def parseMessage(data):
 	parsedMessage = ""
 	lenData = len(data)
@@ -29,10 +31,55 @@ def parseMessage(data):
 	return parsedMessage
 
 
+def hash1(val):
+	result = (val + 3) % 1000000007
+	return result
+
+
+def hash2(val):
+	result = (105 * val + 1) % 1000000007
+	return result
+
+
+def hash3(val):
+	result = (1017 * val + 1) % 1000000009
+	return result
+
+
+def hash4(val):
+	result = (1439 * val + 1) % 786543
+	return result
+
+
+def hash5(val):
+	result = (5821*val + 1) % 456783
+	return result 
+
+
+#using Min hash algorithm to find signature matrix
+def findsignatureMatrix(data , args):
+	rows = len(data)
+	cols = len(data[0])
+	sigrows = len(args)
+	#print(sigrows)
+	signatureMatrix = []
+	for _ in range(sigrows):
+		signatureMatrix.append([9999999999] * cols)
+	for row in range(rows):
+		 Hash = map(lambda x : x(row) , args)
+		 for col in range(cols):
+		 	if(data[row][col] != 0):
+		 		for idx in range(sigrows):
+		 			if(signatureMatrix[idx][col] > Hash[idx]):
+		 				signatureMatrix[idx][col] = Hash[idx]
+	return signatureMatrix
+
+
+
 UniqueShingles = set()
 MatrixBeg = []
 count = 0
-for i in range(1,4):
+for i in range(1 , 4):
 	FileName = "test" + str(i) + ".txt"
 	#print(FileName)
 	readtextFile = open(FileName,'r')
@@ -51,7 +98,7 @@ for i in range(1,4):
 	for shingle in shingles:
 		countOfShingles += 	1
 		if(shingle not in UniqueShingles):
-			MatrixBeg.append([shingle,count])
+			MatrixBeg.append([shingle , count])
 			count += 1
 			UniqueShingles.add(shingle)
 		writeTextFile.write(shingle)
@@ -59,7 +106,52 @@ for i in range(1,4):
 	#print(countOfShingles) #2353 2725 2077
 	writeTextFile.close()
 #print(len(UniqueShingles))
-print(len(MatrixBeg))
-charactersticMatrix = []
+#print(len(MatrixBeg))
+#print(MatrixBeg)
+charactersticMatrix = [[0 for x in range(4)] for y in range(len(MatrixBeg) + 1)] 
+
+#print(charactersticMatrix)
+for i in range(1 , 4):
+	FileName = "shingles" + str(i) + ".txt"
+	readtextFile = open(FileName,"r")
+	message = readtextFile.read()
+	for j in range(0 , 6448):
+		#print(MatrixBeg[j])
+		if(MatrixBeg[j][0] in message):
+			charactersticMatrix[j][i] = 1
+	readtextFile.close()
+#for i in charactersticMatrix:
+#	print(i)
+#	print("\n")
+#print(charactersticMatrix)
+
+sigMatrix = findsignatureMatrix(charactersticMatrix, [hash1 , hash2 , hash3 , hash4 , hash5])
+#print(sigMatrix)
+for i in range(len(sigMatrix)):
+	sigMatrix[i][0]=0
+#print(sigMatrix)
+
+
+'''
+counter = 0
+TESTING DONE SHIGLING, CHARACTERSTIC MATRIX AND SIGNATUARE MATRIX ABSOUTELY CORRECT
+for i in charactersticMatrix:
+	print(i)
+	counter += 1
+	print("\n")
+ok = 1
+row = 999
+for i in charactersticMatrix:
+	res = 0
+	for j in range(4):
+		if(i[j] == 1):
+			res += 1
+			if(res == 2):
+				row = i
+				ok = 0
+print(ok)
+print(row)
+print(charactersticMatrix)
 for i in MatrixBeg:
 	print(i)
+'''
